c02af2dc49722a17c37f224c031b1ed88c61ba8c
hyezuu <duudu0704@gmail.com>
2025-02-13 14:52:41 +0900

    feat(AreaCode): 엔티티 생성

---
+++ src/main/java/com/ana29/deliverymanagement/entity/AreaCode.java
@@ -0,0 +1,30 @@
+package com.ana29.deliverymanagement.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "p_area_codes")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class AreaCode {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	@Column(name = "area_code_id", columnDefinition = "uuid")
+	private UUID id;
+
+	@Column(length = 10, nullable = false)
+	private String code;
+
+	@Column(nullable = false)
+	private String cityName;
+}
