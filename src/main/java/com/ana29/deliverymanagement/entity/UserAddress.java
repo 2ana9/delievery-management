24b552e8c1d3b5040716c81ea001195ec0c04647
devdaeun <ekdms008@gmail.com>
2025-02-13 13:31:39 +0900

    UserAddress entity 추가

---
+++ src/main/java/com/ana29/deliverymanagement/entity/UserAddress.java
@@ -0,0 +1,42 @@
+package com.ana29.deliverymanagement.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+public class UserAddress {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "uuid")
+    private UUID useraddress_id;
+
+    @Column(nullable = false)
+    private String address;
+
+    @CreatedDate
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime created_at;
+
+    private LocalDateTime updated_at;
+
+    private LocalDateTime deleted_at;
+
+    @Column(nullable = false)
+    private String created_by;
+
+    private String updated_by;
+
+    private String deleted_by;
+
+    //외래키의경우 엔티티 작성완료 후 추가예정
+
+
+}
