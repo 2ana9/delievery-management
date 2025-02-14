372b0156a0992c2638246d01ee2f3fb2e6c6864f
hansg0325 <hansg0325@gmail.com>
2025-02-14 21:29:47 +0900

    #15_LoginAPI에서 pull해서 오류 해결 및 주석 제거.

--- src/main/java/com/ana29/deliverymanagement/security/UserDetailsServiceImpl.java
+++ src/main/java/com/ana29/deliverymanagement/security/UserDetailsServiceImpl.java
@@ -1,24 +1,24 @@
-//package com.ana29.deliverymanagement.security;
-//
-//import com.ana29.deliverymanagement.entity.User;
-//import com.ana29.deliverymanagement.repository.UserRepository;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class UserDetailsServiceImpl implements UserDetailsService {
-//
-//    private final UserRepository userRepository;
-//
-//    @Override
-//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//        User user = userRepository.findById(username)
-//                .orElseThrow(() -> new UsernameNotFoundException("Not Found " + username));
-//
-//        return new UserDetailsImpl(user);
-//    }
-//}
+package com.ana29.deliverymanagement.security;
+
+import com.ana29.deliverymanagement.entity.User;
+import com.ana29.deliverymanagement.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findById(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Not Found " + username));
+
+        return new UserDetailsImpl(user);
+    }
+}
