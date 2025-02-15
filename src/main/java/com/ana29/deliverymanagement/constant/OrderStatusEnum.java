06d2f87d59aad0d9461c4b76b5ae9c4b92ba5076
hyezuu <duudu0704@gmail.com>
2025-02-16 02:09:20 +0900

    feat(order): 주문상태변경가능여부 확인 메서드 추가

--- src/main/java/com/ana29/deliverymanagement/constant/OrderStatusEnum.java
+++ src/main/java/com/ana29/deliverymanagement/constant/OrderStatusEnum.java
@@ -4,14 +4,22 @@ import lombok.Getter;
 
 @Getter
 public enum OrderStatusEnum {
-    PENDING("주문 완료"),
+    PENDING("주문 접수"),
     PAID("결제 완료"),
-    SHIPPED("배송 중"),
-    CANCELED("취소됨");
+    COMPLETED("주문 완료"),
+    CANCELED("주문 취소");
 
     private final String description;
 
     OrderStatusEnum(String description) {
         this.description = description;
+    }
+
+    public boolean canChangeTo(OrderStatusEnum nextStatus) {
+        return switch (this) {
+            case PENDING -> nextStatus == PAID || nextStatus == CANCELED;
+            case PAID -> nextStatus == COMPLETED || nextStatus == CANCELED;
+            case COMPLETED,CANCELED -> false;
+        };
     }
 }
