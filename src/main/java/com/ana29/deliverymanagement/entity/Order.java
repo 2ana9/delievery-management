57eaade231597531953aca3cd902293254e80b3f
hyezuu <duudu0704@gmail.com>
2025-02-15 18:18:26 +0900

    fix(order): order entity 수정

--- src/main/java/com/ana29/deliverymanagement/entity/Order.java
+++ src/main/java/com/ana29/deliverymanagement/entity/Order.java
@@ -1,10 +1,25 @@
 package com.ana29.deliverymanagement.entity;
 
 import com.ana29.deliverymanagement.constant.OrderStatusEnum;
-import jakarta.persistence.*;
-import lombok.*;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 
 @Entity
 @Getter
@@ -12,31 +27,32 @@ import java.util.UUID;
 @AllArgsConstructor(access = AccessLevel.PRIVATE) // 빌더를 통한 생성만 허용
 @Builder
 @Table(name = "p_order")
-class Order extends Timestamped{
+public class Order extends Timestamped{
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "order_id", columnDefinition = "uuid")
     private UUID id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "menu_id", nullable = false)
+    private Menu menu;
+
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private OrderStatusEnum orderStatus;
-
-    @Column(length = 100)
-    private String content;
-
-    @Column(length = 100, nullable = false)
-    private String operatingHours;
-
-    @Column(length = 100, nullable = false)
-    private String orderRequest;
-
-    @Column(length = 255, nullable = false)
-    private String address;
-
-    @Column(length = 255, nullable = false)
-    private int price;
+    @Builder.Default
+    private OrderStatusEnum orderStatus = OrderStatusEnum.PENDING;
 
     @Column(nullable = false)
-    private UUID payment_id;
+    private Integer quantity;
+
+    @Column(length = 100)
+    private String orderRequest;
+
+    public Long getTotalAmount() {
+        return this.menu.getPrice() * quantity;
+    }
 }
