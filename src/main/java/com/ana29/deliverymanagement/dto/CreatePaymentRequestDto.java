bae8cf39a30b6d796f1f0c12b3e40c7a23a76e99
hyezuu <duudu0704@gmail.com>
2025-02-17 11:34:24 +0900

    fix(order): 주문 생성 로직 수정 - 결제와 하나의 트랜잭션으로

--- src/main/java/com/ana29/deliverymanagement/dto/CreatePaymentRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.ana29.deliverymanagement.dto;
-
-import jakarta.validation.constraints.NotNull;
-import java.util.UUID;
-
-public record CreatePaymentRequestDto (@NotNull UUID orderId,
-									   @NotNull String paymentType) {
-
-}
