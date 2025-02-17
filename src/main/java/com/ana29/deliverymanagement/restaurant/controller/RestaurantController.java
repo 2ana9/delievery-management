75ccee23ddf84a5898d1bb6e2ae5fcb18d20ce18
devdaeun <ekdms008@gmail.com>
2025-02-17 14:06:20 +0900

    import 작성오류 수정

--- src/main/java/com/ana29/deliverymanagement/restaurant/controller/RestaurantController.java
+++ src/main/java/com/ana29/deliverymanagement/restaurant/controller/RestaurantController.java
@@ -7,10 +7,7 @@ import com.ana29.deliverymanagement.security.UserDetailsImpl;
 import com.ana29.deliverymanagement.restaurant.service.RestaurantService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.nio.file.AccessDeniedException;
 
@@ -21,13 +18,28 @@ public class RestaurantController {
 
     private final RestaurantService restaurantService;
 
-    //
+    //가게 추가 메소드(관리자)
     @PostMapping
     public RestaurantResponseDto createRestaurant(@RequestBody RestaurantRequestDto restaurantRequestDto,
                                                   @AuthenticationPrincipal UserDetailsImpl userDetails) throws AccessDeniedException {
         checkUserAccess(userDetails);
         return restaurantService.createRestaurant(restaurantRequestDto);
     };
+
+    //가게 수정 메소드(관리자,가게사장)
+//    @PutMapping()
+//    public RestaurantResponseDto updateRestaurant(@RequestBody RestaurantRequestDto restaurantRequestDto
+//            ,@AuthenticationPrincipal UserDetailsImpl userDetails)throws AccessDeniedException{
+//      //수정은 관리자도 가능하고 가게사장도 가능하게 구현
+//        UserRoleEnum userRole = userDetails.getUser().getRole();
+//        if (userRole != UserRoleEnum.ADMIN && userRole != UserRoleEnum.) {
+//            throw new AccessDeniedException("관리자 접근이 필요합니다.");
+//        }
+//    };
+
+    //가게 조회 메소드 (전체)
+
+    //가게 삭제메소드(관리자,가게사장)
 
     //사용자의 권한확인 메소드
     public void checkUserAccess(UserDetailsImpl userDetails) throws AccessDeniedException {
