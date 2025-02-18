0eef7cdd448ba45ddf580467bf620ad96a9529f6
moon <ko5642027@naver.com>
2025-02-18 19:44:27 +0900

    - develop2 앱이 정상적으로 실행 될수 있도록 수정

--- src/main/java/com/ana29/deliverymanagement/area/repository/AreaSearchRepository.java
+++ src/main/java/com/ana29/deliverymanagement/area/repository/AreaSearchRepository.java
@@ -6,10 +6,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
-public interface AreaSearchRepository extends ElasticsearchRepository<AreaDocument, String> {
-    // 커스텀 쿼리 메서드 (복합적인 검색을 위해)
-    @Query("{\"bool\": {\"should\": ["
-            + "{\"match\": {\"fullAddressTown\": \"?0\"}}, "
-            + "{\"match\": {\"fullAddressRoadName\": \"?0\"}} ]}}")
-    Page<AreaDocument> findByFullAddressContaining(String search, Pageable pageable);
-}
+//public interface AreaSearchRepository extends ElasticsearchRepository<AreaDocument, String> {
+//    // 커스텀 쿼리 메서드 (복합적인 검색을 위해)
+//    @Query("{\"bool\": {\"should\": ["
+//            + "{\"match\": {\"fullAddressTown\": \"?0\"}}, "
+//            + "{\"match\": {\"fullAddressRoadName\": \"?0\"}} ]}}")
+//    Page<AreaDocument> findByFullAddressContaining(String search, Pageable pageable);
+//}
