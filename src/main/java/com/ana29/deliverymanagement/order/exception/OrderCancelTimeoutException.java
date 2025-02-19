778c91962721e473911097edaa3b9fd593f881dd
hyezuu <duudu0704@gmail.com>
2025-02-19 12:55:24 +0900

    fix(exception): 예외 상속

--- src/main/java/com/ana29/deliverymanagement/order/exception/OrderCancelTimeoutException.java
+++ src/main/java/com/ana29/deliverymanagement/order/exception/OrderCancelTimeoutException.java
@@ -1,6 +1,8 @@
 package com.ana29.deliverymanagement.order.exception;
 
-public class OrderCancelTimeoutException extends RuntimeException {
+import com.ana29.deliverymanagement.global.exception.CustomConflictException;
+
+public class OrderCancelTimeoutException extends CustomConflictException {
 
 	public OrderCancelTimeoutException() {
 		super("주문 취소는 결제 후 5분 이내에만 가능합니다.");
