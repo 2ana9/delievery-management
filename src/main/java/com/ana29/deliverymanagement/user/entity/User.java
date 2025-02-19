06c4e1daa100be8d060ed4a2056af703d7525708
moon <ko5642027@naver.com>
2025-02-19 14:03:53 +0900

    - create 추가

--- src/main/java/com/ana29/deliverymanagement/user/entity/User.java
+++ src/main/java/com/ana29/deliverymanagement/user/entity/User.java
@@ -4,6 +4,7 @@ import com.ana29.deliverymanagement.global.entity.Timestamped;
 import com.ana29.deliverymanagement.user.constant.user.UserRoleEnum;
 import jakarta.persistence.*;
 import lombok.*;
+import java.util.*;
 
 
 @Entity
@@ -37,4 +38,12 @@ public class User extends Timestamped {
     @Column(length = 255, nullable = true)
     private String currentAddress; // 대표 주소
 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<UserAddress> addresses = new ArrayList<>();
+
+    // 주소 추가 메서드
+    public void addAddress(UserAddress address) {
+        addresses.add(address);
+        address.setUser(this);
+    }
 }
