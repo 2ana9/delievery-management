158c42a2220c2654c23c44c8ced066f7693f52b5
yng <namgyu967@gmail.com>
2025-02-21 16:11:45 +0900

    - Merge

--- src/main/java/com/ana29/deliverymanagement/user/repository/UserAddressRepository.java
+++ src/main/java/com/ana29/deliverymanagement/user/repository/UserAddressRepository.java
@@ -1,12 +1,13 @@
 package com.ana29.deliverymanagement.user.repository;
 
+import com.ana29.deliverymanagement.user.entity.User;
 import com.ana29.deliverymanagement.user.entity.UserAddress;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
-public interface UserAddressRepository extends JpaRepository<UserAddress, String> {
-//    List<UserAddress> findByUser(User user);
-
-    Optional<UserAddress> findById(String id);
+public interface UserAddressRepository extends JpaRepository<UserAddress, UUID>, UserAddressRepositoryCustom {
+    List<UserAddress> findByUserAndIsDeletedFalse(User user);
+    Optional<UserAddress> findByIdAndUserIdAndIsDeletedFalse(UUID id, String userId);
 }
