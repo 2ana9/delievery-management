6c911a37aa0cddbca5ec47e60b78858d55bed3ff
hyezuu <duudu0704@gmail.com>
2025-02-23 00:45:34 +0900

    fix(exception): 예외 처리, 사용되지 않는 예외 삭제

--- src/main/java/com/ana29/deliverymanagement/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ana29/deliverymanagement/global/exception/GlobalExceptionHandler.java
@@ -3,12 +3,15 @@ package com.ana29.deliverymanagement.global.exception;
 import com.ana29.deliverymanagement.global.dto.ResponseDto;
 import com.ana29.deliverymanagement.order.exception.MissingUserAddressException;
 import com.ana29.deliverymanagement.order.exception.PaymentFailException;
+import com.ana29.deliverymanagement.review.exception.AlreadyReviewedException;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -49,15 +52,17 @@ public class GlobalExceptionHandler {
 			"Type MethodArgumentNotValidExceptionHandler : " + errorMessage,
 			request);
 	}
+
 	@ExceptionHandler({NullPointerException.class})
-	public ResponseEntity<ResponseDto> nullPointerExceptionHandler(NullPointerException ex, HttpServletRequest request) {
+	public ResponseEntity<ResponseDto> nullPointerExceptionHandler(NullPointerException ex,
+		HttpServletRequest request) {
 		return handleRedirectException("Type nullPointerExceptionHandler : " + ex.getMessage(),
-				request);
+			request);
 	}
 
 	// 공통 로직: 특정 URL (예: 회원가입)인 경우, 에러 메시지를 인코딩하여 리다이렉트 응답 생성
 	private ResponseEntity<ResponseDto> handleRedirectException(String errorMessage,
-																HttpServletRequest request) {
+		HttpServletRequest request) {
 //      에러 메시지를 URL 인코딩하여 쿼리 파라미터로 전달 (예: ?error=메시지)
 //      인코딩 없을 시 url을 해석하지 못하여 403 에러
 		String encodedErrorMessage = URLEncoder.encode(errorMessage, StandardCharsets.UTF_8);
@@ -72,7 +77,8 @@ public class GlobalExceptionHandler {
 		// 이전 페이지 저장
 		String referer = request.getHeader("Referer");
 		// redirecturl 이전 페이지 값
-		String redirectUrl = (referer != null) ? referer + "?error=" + encodedErrorMessage : "/?error=" + encodedErrorMessage;
+		String redirectUrl = (referer != null) ? referer + "?error=" + encodedErrorMessage
+			: "/?error=" + encodedErrorMessage;
 
 		// 회원가입 URL로 시작하는 경우로 제한 (예: "/api/users/sign-up")
 		if (request.getRequestURI().startsWith("/api/users")) {
@@ -105,83 +111,59 @@ public class GlobalExceptionHandler {
 
 	@ExceptionHandler(CustomAccessDeniedException.class)
 	@ResponseBody
-	public ResponseEntity<ResponseDto> handleCustomAccessDeniedException(CustomAccessDeniedException e) {
+	public ResponseEntity<ResponseDto> handleCustomAccessDeniedException(
+		CustomAccessDeniedException e) {
 
 		return ResponseEntity.status(HttpStatus.FORBIDDEN)
 			.body(ResponseDto.failure(HttpStatus.FORBIDDEN, e.getMessage()));
 	}
 
 	@ExceptionHandler(PaymentFailException.class)
-    @ResponseBody
-    public ResponseEntity<ResponseDto> handlePaymentFailException(PaymentFailException e) {
+	@ResponseBody
+	public ResponseEntity<ResponseDto> handlePaymentFailException(PaymentFailException e) {
 
 		return ResponseEntity.status(HttpStatus.PAYMENT_REQUIRED)
 			.body(ResponseDto.failure(HttpStatus.PAYMENT_REQUIRED, e.getMessage()));
-    }
+	}
 
 	@ExceptionHandler(MissingUserAddressException.class)
 	@ResponseBody
-	public ResponseEntity<ResponseDto> handleMissingUserAddressException(MissingUserAddressException e) {
+	public ResponseEntity<ResponseDto> handleMissingUserAddressException(
+		MissingUserAddressException e) {
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
 			.body(ResponseDto.failure(HttpStatus.BAD_REQUEST, e.getMessage()));
 	}
 
+	@ExceptionHandler(AlreadyReviewedException.class)
+	@ResponseBody
+	public ResponseEntity<ResponseDto> handleAlreadyReviewedException(AlreadyReviewedException e) {
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+			.body(ResponseDto.failure(HttpStatus.BAD_REQUEST, e.getMessage()));
+	}
+
+	@ExceptionHandler(ConstraintViolationException.class)
+	@ResponseBody
+	public ResponseEntity<ResponseDto> handleConstraintViolationException(
+		ConstraintViolationException e) {
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+			.body(ResponseDto.failure(HttpStatus.BAD_REQUEST, e.getMessage()));
+	}
+
+	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+	@ResponseBody
+	public ResponseEntity<ResponseDto> handleHttpRequestMethodNotSupportedException(
+		HttpRequestMethodNotSupportedException e) {
+		return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED)
+			.body(ResponseDto.failure(HttpStatus.METHOD_NOT_ALLOWED, e.getMessage()));
+	}
+
+	@ExceptionHandler(Exception.class)
+	@ResponseBody
+	public ResponseEntity<ResponseDto> handleException(Exception e) {
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+			.body(ResponseDto.failure(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다."));
+	}
 
 }
-
-//
-//    @ExceptionHandler({ProductNotFoundException.class})
-//    @ResponseBody
-//    public ResponseEntity<ExceptionResponse> notFoundProductExceptionHandler
-//    (ProductNotFoundException ex) {
-//        ExceptionResponse response = ExceptionResponse.of(ex.getMessage(), HttpStatus.NOT_FOUND
-//        .value());
-//        return new ResponseEntity<>(
-//                // HTTP body
-//                response,
-//                // HTTP status code
-//                HttpStatus.NOT_FOUND
-//        );
-//    }
-//
-//
-//
-//    @ExceptionHandler(AlreadyReviewedException.class)
-//    @ResponseBody
-//    public ResponseEntity<ExceptionResponse> handleAlreadyReviewedException
-//    (AlreadyReviewedException e) {
-//        ExceptionResponse response = ExceptionResponse.of(e.getMessage(), HttpStatus
-//        .BAD_REQUEST.value());
-//        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-//    }
-//
-//    //@Valid로 검증할 때 발생하는 예외
-//    @ExceptionHandler(MethodArgumentNotValidException.class)
-//    @ResponseBody
-//    public ResponseEntity<ExceptionResponse> handleMethodArgumentNotValidException
-//    (MethodArgumentNotValidException e) {
-//        ExceptionResponse response = ExceptionResponse.of(e.getBindingResult());
-//        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-//    }
-//
-//    //@Validated와 함께 사용되는 메서드 파라미터 검증 실패 시 발생하는 예외
-//    @ExceptionHandler(ConstraintViolationException.class)
-//    @ResponseBody
-//    public ResponseEntity<ExceptionResponse> handleConstraintViolationException
-//    (ConstraintViolationException e) {
-//        ExceptionResponse response = ExceptionResponse.of(e.getConstraintViolations());
-//        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-//    }
-//
-//    //지원하지 않는 HTTP 메서드로 요청이 왔을 때 발생하는 예외
-//    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-//    @ResponseBody
-//    public ResponseEntity<ExceptionResponse> handleHttpRequestMethodNotSupportedException
-//    (HttpRequestMethodNotSupportedException e) {
-//        ExceptionResponse response = ExceptionResponse.of(e.getMessage(), HttpStatus
-//        .METHOD_NOT_ALLOWED.value());
-//        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(response);
-//    }
-//
 
 
